38403bdb61fc484d9f15fb7caae8eede2c607014
RognGit-Repo <ronald.reyes.me@gmail.com>
2024-03-19 22:50:22 +0800

    Routes Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import "./globals.css";
-
+import { Routes, Route } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import { Home } from "./_root/Index";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 const App = () => {
-  return <div className="text-3x1 font-bold underline">App</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+
+        {}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
